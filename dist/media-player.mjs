ffaa485860d6d27ad1ceaa11e44779afbd3c1fdf
Nikola Stamatovic <stamatmail@gmail.com>
2026-08-31 16:06:52 +0200

    Build version 2.1.0

--- dist/media-player.mjs
+++ dist/media-player.mjs
@@ -1,4 +1,4 @@
-/* media-player-element v2.0.0 | https://stamat.github.io/media-player-element/ | MIT License */
+/* media-player-element v2.1.0 | https://stamat.github.io/media-player-element/ | MIT License */
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
